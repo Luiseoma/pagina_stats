9ab7ea4ef217d0417920dc273f12c1b69bd854cb
luiseoma <luisenriqueoma@gmail.com>
2026-05-19 20:16:56 -0400

    agregado probabilidaes

--- app.py
+++ app.py
@@ -2,17 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+def calcular_fuerza(form):
+    return sum(form) / len(form)
+
 matches = {
     1: {
-        'team1': 'México',
-        'team2': 'Sudáfrica',
-        'probabilities': {
-            'team1': 55,
-            'draw': 25,
-            'team2': 20,
-        },
-        'link': 'https://example.com/bet'
-
+        "team1": "México",
+        "team2": "Sudáfrica",
+        "form_team1": [1, 0, 1, 1, 1],
+        "form_team2": [0, 1, 0, 0, 1]
     }
 }
 
@@ -20,12 +18,24 @@ matches = {
 def home ():
     return render_template('home.html',matches=matches)
 
-@app.route('/match/<int:id>)')
-def match (id):
-    match = matches.get (id)
-    return render_template('match.html',match=match)
+@app.route("/match/<int:id>")
+def match(id):
+    match = matches[id]
 
-if __name__ == "__main__":
-    app.run(debug=True)
+    f1 = calcular_fuerza(match["form_team1"])
+    f2 = calcular_fuerza(match["form_team2"])
 
+    total = f1 + f2
+
+    prob_team1 = round((f1 / total) * 100, 1)
+    prob_team2 = round((f2 / total) * 100, 1)
+    prob_draw = round(100 - (prob_team1 + prob_team2), 1)
+
+    return render_template(
+        "match.html",
+        match=match,
+        prob_team1=prob_team1,
+        prob_team2=prob_team2,
+        prob_draw=prob_draw
+    )
     
